75f34e7602b7fbac4d7e3a51a4ca1fa8135bb415
khadija <khadijalahmar8@gmail.com>
2022-01-19 00:26:14 +0100

    create UserSchema in model

---
+++ model/UserModel.js
@@ -0,0 +1,24 @@
+const mongoose =require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username : {
+        type: String,
+        required: true,
+        maxlength: 20
+    },
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        maxlength: 60
+    },
+    password : {
+        type: String,
+        required: true
+    }
+});
+
+// creating the model
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User
