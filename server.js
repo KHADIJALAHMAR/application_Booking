db9b250abe34f015f1ba8423345fed5868d462c5
khadija <khadijalahmar8@gmail.com>
2022-01-18 23:56:02 +0100

    create UserController and require fil server.js

--- server.js
+++ server.js
@@ -2,6 +2,20 @@ const express = require('express');
 const app =express();
 const path = require('path');
 
+// require controller user
+const UserController = require('./controller/userController');
+// import fil connexion database
+require('./config/database');
+// 
+
+
+// template engine
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine', 'ejs');
+
+// encoded url 
+
+app.use(express.urlencoded({extended: true}));
 
 
 
