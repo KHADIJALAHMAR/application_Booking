8a19528b33df0c1dfe53faec9ba791c6f2565de6
khadija <khadijalahmar8@gmail.com>
2022-01-23 00:44:53 +0100

    create route validation login

--- routes/routeUser.js
+++ routes/routeUser.js
@@ -8,7 +8,7 @@ const UserController = require('../controllers/userController');
 
 router.route('/login')
 .get(UserController.loadLoginPage )
-.post();
+.post(UserController.validation_login);
 router.route('/register')
 .get(UserController.loadRegisterPage )
 .post(UserController.validation_register);
